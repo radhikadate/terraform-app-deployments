892e91719caa9e3804b0033f42a53ea2be59587f
radhikadate <radhika.date1012@gmail.com>
2026-01-09 20:00:56 +0530

    Add NAT Gateway for private subnet internet access

--- deployment/common-modules/network-multi-az/main.tf
+++ deployment/common-modules/network-multi-az/main.tf
@@ -57,3 +57,41 @@ resource "aws_route_table_association" "public_subnet_associations" {
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_rtb.id
 }
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project}-${var.environment}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "${var.project}-${var.environment}-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-private-rtb"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_associations" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_rtb.id
+}
