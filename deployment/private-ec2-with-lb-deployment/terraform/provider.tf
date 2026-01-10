6898d4e54e2b1a809068c16012fb6d910bb05ff5
radhikadate <radhika.date1012@gmail.com>
2026-01-11 01:25:34 +0530

    Add S3 backend for Terraform state

--- deployment/private-ec2-with-lb-deployment/terraform/provider.tf
+++ deployment/private-ec2-with-lb-deployment/terraform/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "6.24.0"
     }
   }
+  
+  backend "s3" {
+    bucket         = "terraform-state-radhika-deployments"
+    key            = "private-ec2-alb/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
