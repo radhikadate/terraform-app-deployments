6832d06fd5df8c359818d1512ac97a4e2e3f76bc
radhikadate <radhika.date1012@gmail.com>
2026-01-30 16:03:42 +0530

    feat: Add Terraform configuration for deploying public EC2 instances with an Application Load Balancer, including a compute module and a GitHub Actions workflow.

--- deployment/common-modules/compute-multi-instance-public-subnet/variables.tf
+++ deployment/common-modules/compute-multi-instance-public-subnet/variables.tf
@@ -13,11 +13,6 @@ variable "public_subnet_ids" {
   type        = list(string)
 }
 
-variable "private_subnet_ids" {
-  description = "List of private subnet IDs for EC2 instances"
-  type        = list(string)
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
